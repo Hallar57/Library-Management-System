52cd138125665ff003088231cf0b480c3ff8b1b4
Hallar <161750160+Hallar57@users.noreply.github.com>
2025-06-05 16:00:22 +0000

    books on homepage

--- LibraryWeb/scripts/homepage.js
+++ LibraryWeb/scripts/homepage.js
@@ -1,20 +1,24 @@
-const JOB_API_LINK =  "https://curly-invention-r47rr5q756p7cp9x4-5001.app.github.dev/books";
+const books_API_LINK =  "https://curly-invention-r47rr5q756p7cp9x4-5001.app.github.dev/books";
 
-fetch(JOB_API_LINK).then(response=>{
+fetch(books_API_LINK).then(response=>{
     if(!response.ok)
         throw new Error("Failed to fetch data");
     return response.json();
 }).then(data=>{
-    const tbody = document.querySelector("#jobtable tbody");
+    const tbody = document.querySelector("#bookstable tbody");
 
-    data.forEach(job=>{
+    data.forEach(books=>{
         const row = document.createElement("tr");
 
         row.innerHTML=`
-        <td>${job.book_id}</td>
-        <td>${job.job_title}</td>
-        <td>${job.min_salary}</td>
-        <td>${job.max_salary}</td>
+        <td>${books.book_id}</td>
+        <td>${books.title}</td>
+        <td>${books.author_id}</td>
+        <td>${books.publisher_id}</td>
+        <td>${books.category_id}</td>
+        <td>${books.isbn}</td>
+        <td>${books.published_year}</td>
+        <td>${books.available_copies}</td>
         `;
         tbody.appendChild(row);
     });
